5b25a612dcdb493987fc89818e1e4e6f3934a3eb
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2025-05-11 17:16:26 -0400

    Add portfolio content

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,91 @@
-import './App.css'
+import Timeline from './components/Timeline.tsx'
+import Event from './components/Event.tsx'
+import resume from './data/resume.json'
+import headshot from './assets/headshot.jpeg'
+import daisy from './assets/daisy.jpg'
 
 function App() {
   return (
-    <section className="bg-white">
-      <div className="py-8 px-4 mx-auto max-w-screen-md text-center lg:py-16 lg:px-12">
-        <svg className="mx-auto mb-4 w-10 h-10 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M331.8 224.1c28.29 0 54.88 10.99 74.86 30.97l19.59 19.59c40.01-17.74 71.25-53.3 81.62-96.65c5.725-23.92 5.34-47.08 .2148-68.4c-2.613-10.88-16.43-14.51-24.34-6.604l-68.9 68.9h-75.6V97.2l68.9-68.9c7.912-7.912 4.275-21.73-6.604-24.34c-21.32-5.125-44.48-5.51-68.4 .2148c-55.3 13.23-98.39 60.22-107.2 116.4C224.5 128.9 224.2 137 224.3 145l82.78 82.86C315.2 225.1 323.5 224.1 331.8 224.1zM384 278.6c-23.16-23.16-57.57-27.57-85.39-13.9L191.1 158L191.1 95.99l-127.1-95.99L0 63.1l96 127.1l62.04 .0077l106.7 106.6c-13.67 27.82-9.251 62.23 13.91 85.39l117 117.1c14.62 14.5 38.21 14.5 52.71-.0016l52.75-52.75c14.5-14.5 14.5-38.08-.0016-52.71L384 278.6zM227.9 307L168.7 247.9l-148.9 148.9c-26.37 26.37-26.37 69.08 0 95.45C32.96 505.4 50.21 512 67.5 512s34.54-6.592 47.72-19.78l119.1-119.1C225.5 352.3 222.6 329.4 227.9 307zM64 472c-13.25 0-24-10.75-24-24c0-13.26 10.75-24 24-24S88 434.7 88 448C88 461.3 77.25 472 64 472z"/></svg>
-        <h1 className="mb-4 text-4xl font-bold tracking-tight leading-none text-gray-900 lg:mb-6 md:text-5xl xl:text-6xl dark:text-white">Under Maintenance</h1>
-        <p className="font-light text-gray-500 md:text-lg xl:text-xl dark:text-gray-400">Come back soon! - Steph</p>
+    <section className="bg-white dark:bg-gray-900 antialiased">
+      <div className="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+        <div className="max-w-4xl mx-auto text-center">
+          <img className="rounded-full w-50 h-50 mx-auto" src={headshot} alt="image description" />
+          <h1 className="mb-2 sm:mb-4 text-4xl font-extrabold text-gray-900 dark:text-white md:text-5xl">
+            Hello, I'm{' '}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              Steph Chamblee
+            </span>
+            !
+          </h1>
+          <p className="text-lg font-normal text-gray-600 lg:text-xl dark:text-gray-400 mb-12">
+            Welcome to my resume 👋.
+          </p>
+
+          <div className='flex flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mb-12'>
+            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-60 md:rounded-none md:rounded-s-lg" src={daisy} alt="my dog Daisy" />
+            <div className="flex flex-col justify-between leading-normal">
+              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">About Me</h2>
+              <p className="mb-3 font-normal text-base text-gray-700 dark:text-gray-400 px-4">
+                I'm a senior full-stack engineer with over seven years of experience building secure, scalable web applications that are genuinely useful. I care a lot about clean architecture, accessibility, and reducing friction through thoughtful collaboration and problem-solving.
+              </p>
+              <p className="font-normal text-gray-700 dark:text-gray-400 px-4 pb-2">
+                Outside of work, I'm a new dog parent to Daisy (see left), a sweet pitbull mix who I've been spending most of my time with lately. I'm also into board games, video games, puzzles, and other nerdy stuff 😁.
+              </p>
+              <p className="font-normal text-gray-700 dark:text-gray-400 px-4 pb-2">
+                I’m currently looking for my next opportunity as a senior engineer where I can continue to grow while helping a team ship high-quality, well-crafted software.
+              </p>
+            </div>
+          </div>
+
+          <Timeline heading="Work Experience">
+            {resume['experience'].map((e) => (
+              <Event
+                startDate={new Date(e['startDate'])}
+                endDate={new Date(e['endDate'])}
+                title={e['title']}
+                highlights={e['highlights']} />
+            ))}
+          </Timeline>
+          <Timeline heading="Education">
+            {resume['education'].map((e) => (
+              <Event
+                startDate={new Date(e['startDate'])}
+                endDate={new Date(e['endDate'])}
+                title={e['degree']}
+                highlights={[e['institution']]} />
+            ))}
+          </Timeline>
+          <Timeline heading="Certifications">
+            {resume['certifications'].map((e) => (
+              <Event
+                startDate={new Date(e['startDate'])}
+                endDate={new Date(e['endDate'])}
+                title={e['title']}
+                highlights={[e['provider']]} />
+            ))}
+          </Timeline>
+          <Timeline heading="Projects">
+            {resume['projects'].map((e) => (
+              <Event
+                startDate={new Date(e['startDate'])}
+                endDate={new Date(e['endDate'])}
+                title={e['title']}
+                highlights={[e['organization'], e['description']]} />
+            ))}
+          </Timeline>
+          <Timeline heading="Conference Talks">
+            {resume['publications'].map((e) => (
+              <Event
+                startDate={new Date(e['startDate'])}
+                endDate={new Date(e['endDate'])}
+                title={e['title']}
+                highlights={e['events']} />
+            ))}
+          </Timeline>
+        </div>
       </div>
-   </section>
-  );
+    </section>
+  )
 }
 
 export default App
---
+++ src/components/Event.tsx
@@ -0,0 +1,31 @@
+interface EventProps {
+  startDate: Date
+  endDate: Date
+  title: string
+  highlights: Array<string>
+}
+
+function Event({ startDate, endDate, title, highlights }: EventProps) {
+  return (
+    <li className="mb-10 ms-4">
+      <div className="absolute w-3 h-3 group-odd:bg-sky-300 group-even:bg-emerald-600 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+      <time className="mb-1 text-sm font-normal leading-none text-gray-600 dark:text-gray-600">
+        {startDate.toLocaleDateString()}
+      </time>
+      <span className="text-sm font-normal text-gray-600 dark:text-gray-600">–</span>
+      <time className="mb-1 text-sm font-normal leading-none text-gray-600 dark:text-gray-600">
+        {endDate.toLocaleDateString()}
+      </time>
+      <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{title}</h3>
+      <ul>
+        {highlights.map((highlight, i) => (
+          <li className="mb-4 text-base font-normal text-gray-600 dark:text-gray-400" key={i}>
+            {highlight}
+          </li>
+        ))}
+      </ul>
+    </li>
+  )
+}
+
+export default Event
