341851bb928a40e90c3c1952eb0c5808dc20c53c
Stephanie Chamblee <stephaniejoychamblee@gmail.com>
2025-05-11 23:24:36 -0400

    Styling fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow-sm sm:flex sm:items-center sm:justify-between sm:p-6 dark:bg-gray-800 dark:border-gray-600">
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © {new Date().getFullYear()} Stephanie Chamblee. All Rights Reserved.
       </span>
-      <div className="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
+      <div className="flex mt-2 sm:mt-4 justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
         <a
           href="mailto:stephaniejoychamblee@gmail.com?subject=Let's Schedule an Interview!"
           className="text-gray-500 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
@@ -13,7 +13,7 @@ function Footer() {
             aria-hidden={true}
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
-            className="size-6"
+            className="size-5 sm:size-6"
             viewBox="0 0 16 16"
           >
             <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2zm-2 9.8V4.698l5.803 3.546zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.5 4.5 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586zM16 9.671V4.697l-5.803 3.546.338.208A4.5 4.5 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671" />
@@ -29,7 +29,7 @@ function Footer() {
             aria-hidden={true}
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
-            className="size-6"
+            className="size-5 sm:size-6"
             viewBox="0 0 16 16"
           >
             <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
@@ -44,7 +44,7 @@ function Footer() {
             aria-hidden={true}
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
-            className="size-6"
+            className="size-5 sm:size-6"
             viewBox="0 0 16 16"
           >
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
